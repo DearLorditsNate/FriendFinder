2d9c44669a8e1e69745f8d2d7aacebce927952e9
Nate Micinski <nmicinski@gmail.com>
2019-03-16 20:14:53 -0700

    update API paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,5 +7,20 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         friends.push(req.body);
+
+        // Matching logic
+        function sum(total, num) {
+            return total + num;
+        }
+
+        // If only one friend in arr, return that person
+        var bestMatch = friends[0];
+
+        // 
+        // for (var i = 0; i < friends.length; i++) {
+        //     if (Math.abs(req.body.scores.reduce(sum) - friends[i].scores.reduce(sum)) ) {
+        //         bestMatch = friends[i].scores.reduce(sum);
+        //     }
+        // }
     });
 }
